946862e8e2be34824302811c4513a749bdc15403
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-07-13 00:20:47 +0700

    Update start-train-model.sh allora-train-model:1.0.9

--- allora-node/start-train-model.sh
+++ allora-node/start-train-model.sh
@@ -4,7 +4,13 @@
 export DOCKER_BUILDKIT=1
 
 # Build the Docker image with cache
-docker build -f Dockerfile_train_models -t allora-train-model:1.0.0 .
+docker build -f Dockerfile_train_models -t allora-train-model:1.0.9 .
 
-# Run the container with volume mounts
-docker run -v $(pwd)/models:/app/models -e DATABASE_PATH=/app/data/prices.db -v $(pwd)/source-data:/app/data -d allora-train-model:1.0.0
+# Run the container with volume mounts and Tiingo environment variables
+docker run \
+  -v $(pwd)/models:/app/models \
+  -v $(pwd)/source-data:/app/data \
+  -e TIINGO_API_TOKEN=xxa99de96faaf30fc292a9c5b785974efa85708646 \
+  -e TIINGO_DATA_DIR=/app/data/tiingo_data \
+  -e TIINGO_CACHE_TTL=150 \
+  -d allora-train-model:1.0.9
